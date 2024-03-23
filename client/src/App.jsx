bf80f026785519043effd52587909d9c05801d21
Nikhil Dubey <justmailnikhil3@gmail.com>
2024-03-23 13:19:06 +0530

    task 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
+import Card from "./Components/UI/Card";
 
 function App() {
   return (
   <div>
     <Header/>
+    <Meals/>
   </div>
   );
 }
+   
 
 export default App;

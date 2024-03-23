647db67dde8daf6ac7d502cf28a15e23c920d51f
Nikhil Dubey <justmailnikhil3@gmail.com>
2024-03-23 13:30:03 +0530

    task 4

--- client/src/Components/Meals/MealsList.jsx
+++ client/src/Components/Meals/MealsList.jsx
@@ -6,16 +6,28 @@ const MealsList = () => {
   return (
     <div className="grid grid-cols-1">
       {mealsData.map((meal) => (
-        <div key={meal.id} className="">
-          <Card className="bg-white hover:shadow-lg hover:bg-gray-100 transition duration-300 w-72">
-            <ul className="p-4">
-              <li className="text-xl text-red-700 font-semibold mb-2">{meal.name}</li>
-              <li className="text-sm text-gray-600 mb-2">{meal.description}</li>
-              <li className="text-md font-semibold text-red-500">Rs. {meal.price}</li>
+        <Card
+        key={meal.id}
+        className="bg-white hover:shadow-lg hover:bg-gray-100 transition duration-300"
+      >
+        <div className="px-1 py-2 flex justify-between items-center">
+          <div>
+            <ul>
+              <li className="text-xl text-red-700 font-semibold mb-2">
+                {meal.name}
+              </li>
+              <li className="text-sm text-gray-600 mb-2">
+                {meal.description}
+              </li>
+              <li className="text-md font-semibold text-red-500">
+                Rs. {meal.price}
+              </li>
               <li className="text-sm text-red-950">{meal.rating} Stars</li>
             </ul>
-          </Card>
+          </div>
+          <MealsInput />
         </div>
+      </Card>
       ))}
     </div>
   );

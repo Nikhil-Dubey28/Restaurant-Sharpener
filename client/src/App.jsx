9fca56ba7a6800b002c7aa5f88b5459efb71bb5b
Nikhil Dubey <justmailnikhil3@gmail.com>
2024-03-27 19:29:02 +0530

    task 7

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
-import Cart from "./Components/Cart/Cart";
 
 function App() {
   return (
--- client/src/Components/Cart/CartButton.jsx
+++ client/src/Components/Cart/CartButton.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+import Cart from "./Cart";
+
 
 const CartButton = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
